8c57afad23e385fb3d8119436f0365b9d5a4f37d
SrS3R6IO <95187440+SrS3R6IO@users.noreply.github.com>
2022-09-23 10:53:42 +0200

    Update program1.asm

--- first_practice/1/program1.asm
+++ first_practice/1/program1.asm
@@ -4,18 +4,30 @@
     string2: .string "hola"
 
 .text
-main: 
-	syscal 
+main:
+	li s1 0
 string_compare:
     la t0 string1 #Argument 1 = adress 1
     la t1 string2 #Argument 2 = adress 2
     lbu t2 0(t0) # a = char11
     lbu t3 0(t1) # b = char12
 
-    buc1: beqz t2 fin
+buc1: beqz t2 yes
     bne t2 t3 not_eq # if a[n] != b[n] -> not eq
     addi t0 t0 1 # Address1 + 1
     addi t1 t1 1 # Address2 + 1
     lbu t2 0(t0) # reset value of the addresses
     lbu t3 0(t1)
     j buc1
+yes: 
+	li a7 1 #read int
+    li a0 1 #its the same word, so 1
+    ecall
+    li a7 10 #exit
+    ecall
+not_eq:
+	li a7 1
+    li a0 0 #different words, so 0
+    ecall
+    li a7 10 #exit
+    ecall
