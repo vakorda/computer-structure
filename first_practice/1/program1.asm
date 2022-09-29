1a00293c08520ca37d456fb2df2fd99f952eb17f
vakorda <113139381+vakorda@users.noreply.github.com>
2022-09-29 16:57:58 +0200

    Update program1.asm

--- first_practice/1/program1.asm
+++ first_practice/1/program1.asm
@@ -1,16 +1,22 @@
 .data
-  .align 2
+  .align 4
     string1: .string "hola"
-    string2: .string "hola"
+    string2: .string "holas"
 
 .text
 main:
-	li s1 0
+	li a7 1 #read int
+	li a0 -1
+    jal x1 string_compare
+    ecall
+    li a7 10
+    ecall
+    
 string_compare:
     la t0 string1 #Argument 1 = adress 1
     la t1 string2 #Argument 2 = adress 2
-    lbu t2 0(t0) # a = char11
-    lbu t3 0(t1) # b = char12
+    lw t2 0(t0) # a = char11
+    lw t3 0(t1) # b = char12
 
 buc1: beqz t2 try #if t2 is equal to 0, we check if t3 is also equal
     bne t2 t3 not_eq # if a[n] != b[n] -> not eq
@@ -21,16 +27,11 @@ buc1: beqz t2 try #if t2 is equal to 0, we check if t3 is also equal
     j buc1
 try:
 	beqz t3 yes #and if t3 is also equal, both words have the same length so its the same word
-	j not_eq #if not, one word is longer so they arent equal
+	li a0 0 #if not, one word is longer so they arent equal
+    jr ra
 yes: 
-	li a7 1 #read int
     li a0 1 #its the same word, so 1
-    ecall
-    li a7 10 #exit
-    ecall
+    jr ra
 not_eq:
-	li a7 1
-    li a0 0 #different words, so 0
-    ecall
-    li a7 10 #exit
-    ecall
+	li a0 0 #if not, one word is longer so they arent equal
+    jr ra
