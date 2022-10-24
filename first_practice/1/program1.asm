a40f3f6cb1aedfd4f9795f014dc4dae6ee2abef1
vakorda <113139381+vakorda@users.noreply.github.com>
2022-10-24 02:03:03 +0200

    Update program1.asm

--- first_practice/1/program1.asm
+++ first_practice/1/program1.asm
@@ -1,36 +1,40 @@
 .data
   .align 2
-    string1: .string "hola"
-    string2: .string "hola"
+    string1: .string "holar"
+    string2: .string "holas"
 
 .text
 main:
-	li s1 0
+	la a2 string1 #Argument 1 = adress 1
+    la a3 string2 #Argument 2 = adress 2
+	
+    jal ra string_compare # a2 = adress1, a3 = adress2 -> a0 = return
+    
+    # print to test
+    li a7 1 #print int
+    ecall
+    
+    li a7 10 #exit
+    ecall
 string_compare:
-    la t0 string1 #Argument 1 = adress 1
-    la t1 string2 #Argument 2 = adress 2
-    lbu t2 0(t0) # a = char11
-    lbu t3 0(t1) # b = char12
+    la a2 string1 #Argument 1 = adress 1
+    la a3 string2 #Argument 2 = adress 2
+    lbu t2 0(a2) # a = char11
+    lbu t3 0(a3) # b = char12
 
-buc1: beqz t2 try #if t2 is equal to 0, we check if t3 is also equal
+buc1: beqz t2 try # if t2 == 0, we check t3 == 0
     bne t2 t3 not_eq # if a[n] != b[n] -> not eq
-    addi t0 t0 1 # Address1 + 1
-    addi t1 t1 1 # Address2 + 1
-    lbu t2 0(t0) # reset value of the addresses
-    lbu t3 0(t1)
+    addi a2 a2 1 # Address1 + 1
+    addi a3 a3 1 # Address2 + 1
+    lbu t2 0(a2) # load new characters
+    lbu t3 0(a3)
     j buc1
-try:
-	beqz t3 yes #and if t3 is also equal, both words have the same length so its the same word
-	j not_eq #if not, one word is longer so they arent equal
-yes: 
-	li a7 1 #read int
-    li a0 1 #its the same word, so 1
-    ecall
-    li a7 10 #exit
-    ecall
+
+try: beqz t3 yes # if t3 also == 0, both words have the same length and charachters
+	# t3 != 0 one word is longer so they arent equal -> not_eq
 not_eq:
-	li a7 1
     li a0 0 #different words, so 0
-    ecall
-    li a7 10 #exit
-    ecall
+    jr ra
+yes: 
+    li a0 1 #its the same word, so 1
+    jr ra
