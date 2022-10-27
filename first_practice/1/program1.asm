a94db35c5d28aa4e287bdc7d1c3c93e0963e6075
SrS3R6IO <95187440+SrS3R6IO@users.noreply.github.com>
2022-10-27 15:50:40 +0200

    Update program1.asm

--- first_practice/1/program1.asm
+++ first_practice/1/program1.asm
@@ -1,14 +1,14 @@
 .data
   .align 2
-    string1: .string "ayer"
-    string2: .string "aye"
+    string1: .string ""
+    string2: .string ""
 
 .text
 main:
-    la a1 string1 # adress1 = string1
-    la a2 string2 # adress2 = string2
+    la a0 string1 #Argument 1 = adress 1
+    la a1 string2 #Argument 2 = adress 2
 	
-    jal ra string_compare # a1 = adress1, a2 = adress2 -> return = a0
+    jal ra string_compare # a0 = adress1, a2 = adress2 -> return = a0
     
     # print to test
     li a7 1
@@ -16,23 +16,23 @@ main:
     
     li a7 10 #exit
     ecall
-string_compare: # a1 = adress1, a2 = adress2 -> return[1, 0, -1] = a0
-    lbu t2 0(a1) # char1 = adress1[0]
-    lbu t3 0(a2) # char2 = adress2[0]
+string_compare: # a0 = adress1, a2 = adress2 -> return[1, 0, -1] = a0
+    lbu t0 0(a0) # char1 = adress1[0]
+    lbu t1 0(a1) # char2 = adress2[0]
     
     # if char1 == 0 and char2 == 0: error
-    bnez t2 buc1 
-    beqz t3 error
+    bnez t0 buc1 
+    beqz t1 error
 
-buc1: beqz t2 try # if char1 == 0 -> check if char2 == 0
-    bne t2 t3 not_eq # if char1 != char2 -> not eq
-    addi a1 a1 1 # address1 + 1
-    addi a2 a2 1 # address2 + 1
-    lbu t2 0(a1) # set next char into registers
-    lbu t3 0(a2)
+buc1: beqz t0 try # if char1 == 0 -> check if char2 == 0
+    bne t0 t1 not_eq # if char1 != char2 -> not eq
+    addi a0 a0 1 # address1 + 1
+    addi a1 a1 1 # address2 + 1
+    lbu t0 0(a0) # set next char into registers
+    lbu t1 0(a1)
     j buc1
 
-try: bnez t3 not_eq # char2 also == 0: both words have the same length and characters -> return 1
+try: bnez t1 not_eq # char2 also == 0: both words have the same length and characters -> return 1
     li a0 1 # its the same word, so 1
     jr ra
 not_eq:
